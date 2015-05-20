4b069e91cb4674e1b4fbab02eb96dd22a5371bb8
felix <felix.bello@ceforum.ch>
2015-05-20 09:55:17 +0200

    initializing

---
+++ src/MailMover/MailMover/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// Allgemeine Informationen über eine Assembly werden über die folgende 
+// Attributgruppe gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("MailMover")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MailMover")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten. Wenn Sie auf einen Zugriffstyp in dieser Assembly aus 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID ist für die ID der typelib, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("e83e9997-09dc-44ca-8dcc-2e63876d7757")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
---
+++ src/MailMover/MailMover/ThisAddIn.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+
+namespace MailMover
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            
+            this.Application.NewMail += new Microsoft.Office.Interop.Outlook.
+        ApplicationEvents_11_NewMailEventHandler
+        (MoveItem );
+            
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region Von VSTO generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+
+        private void MoveItem() {
+            Outlook.MAPIFolder inBox = (Outlook.MAPIFolder)this.Application.ActiveExplorer().Session.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+            Outlook.Items items = (Outlook.Items )inBox.Items ;
+            Outlook.MailItem moveItem = null;
+            items.Restrict("[UnRead] = true");
+            Outlook.MAPIFolder destinationFolder = inBox.Folders["test"];
+            foreach (object eMail in items)
+            {
+                try
+                {
+                    moveItem = eMail as Outlook.MailItem;
+                    if (moveItem != null)
+                    {
+                        string subject = (string)moveItem.Subject;
+                        if (subject.IndexOf("test")>0)
+                        {
+                            moveItem.Move(destinationFolder );
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    
+                    System.Windows.Forms.MessageBox.Show(ex.Message);
+                }
+            }
+
+
+        }
+        
+        #endregion
+    }
+}
