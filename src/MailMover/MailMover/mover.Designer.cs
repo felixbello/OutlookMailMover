c298c87b40746d27eb856c1032ae76977517ac05
felix <felix.bello@ceforum.ch>
2015-05-20 16:45:58 +0200

    not working yet

---
+++ src/MailMover/MailMover/mover.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace MailMover
+{
+    partial class mover
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // mover
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "mover";
+            this.Text = "mover";
+            this.Load += new System.EventHandler(this.mover_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ src/MailMover/MailMover/mover.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MailMover
+{
+    public partial class mover : Form
+    {
+        public mover()
+        {
+            InitializeComponent();
+        }
+
+        private void mover_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
